5de66b557a4a421d255c12ff26046552872aa5be
Franco Cedillo <franco.cedillo@gmail.com>
2020-03-09 23:58:43 -0500

    move stooq  request to function

--- chat/channels_app/bot.py
+++ chat/channels_app/bot.py
@@ -4,17 +4,21 @@ import csv
 import sys
 
 
+def get_stock(stock_code):
+    response = requests.get(f'https://stooq.com/q/l/?s={stock_code}&f=sd2t2ohlcv&h&e=csv')
+    f = StringIO(response.text)
+    cr = csv.DictReader(f)
+    row = next(cr)
+    if (row['Date'] == 'N/D' and row['Close'] == 'N/D'):
+        return f"stock_code {stock_code} is not defined"
+    else:
+        return f"{row['Symbol']} quote is {row['Close']} per share"
+
+
 def get_stock_quote(stock_code):
     if len(stock_code) > 0:
         try:
-            response = requests.get(f'https://stooq.com/q/l/?s={stock_code}&f=sd2t2ohlcv&h&e=csv')
-            f = StringIO(response.text)
-            cr = csv.DictReader(f)
-            row = next(cr)
-            if (row['Date'] == 'N/D' and row['Close'] == 'N/D'):
-                return f"stock_code {stock_code} is not defined"
-            else:
-                return f"{row['Symbol']} quote is {row['Close']} per share"
+            return get_stock(stock_code)
         except requests.exceptions.Timeout as err:
             # Maybe set up for a retry, or continue in a retry loop
             print(f"Timeout: {err}")
@@ -26,3 +30,4 @@ def get_stock_quote(stock_code):
             print("Unexpected error:", sys.exc_info()[0])
             return "Unexpected Error"
     return "Oh, You misspelled somewhere!"
+
